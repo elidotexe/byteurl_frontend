9874559580145d223d583e1e5008cc70a30768a7
Eli <zoofoo.online@gmail.com>
2023-12-24 23:20:21 +0000

    imploved page loader

--- app/(dashboard)/dashboard/loading.tsx
+++ app/(dashboard)/dashboard/loading.tsx
@@ -1,22 +1,12 @@
-import Link from "next/link";
-
-import { cn } from "@/lib/utils";
-
-import { buttonVariants } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { DashboardShell } from "@/components/dashboard/shell";
-import { Icons } from "@/components/icons";
-import DashboardHeader from "@/components/dashboard/header";
 import LinkItem from "@/components/dashboard/link-item";
 
 const DashboardLoading = () => {
   return (
     <DashboardShell>
-      <DashboardHeader heading="Links" text="Create and manage links.">
-        <Link className={cn(buttonVariants())} href="/dashboard/editor">
-          <Icons.add className="mr-2 h-4 w-4" />
-          New link
-        </Link>
-      </DashboardHeader>
+      <Skeleton className="h-6 w-32" />
+      <Skeleton className="h-4 w-3/4" />
       <div className="divide-border-200 divide-y rounded-md border">
         <LinkItem.Skeleton />
         <LinkItem.Skeleton />
